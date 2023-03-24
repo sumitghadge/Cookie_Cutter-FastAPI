adaddb53f3c7503abc897b17196ef3759dc083dc
sumitghadge <102166756+sumitghadge@users.noreply.github.com>
2023-03-24 14:28:39 +0530

    Update main.py

--- sql_app/main.py
+++ sql_app/main.py
@@ -49,10 +49,3 @@ def login(email: str,password:str, db: Session = Depends(get_db)):
 #     return users
 
 
-# @app.get("/users/{user_id}", response_model=schemas.User)
-# def read_user(user_id: int, db: Session = Depends(get_db)):
-#     db_user = crud.get_user(db, user_id=user_id)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return db_user
-
