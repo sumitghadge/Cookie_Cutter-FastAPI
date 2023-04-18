ade1438ea07f710017cb601de17e002f0bf3ad08
sumitghadge <sghadge@deqode.com>
2023-04-18 11:34:43 +0530

    celery integrated

--- sql_app/main.py
+++ sql_app/main.py
@@ -3,7 +3,8 @@ import os
 from celery import Celery
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -20,9 +21,48 @@ celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 
 
-@celery.task(name="create_task")
-def create_task():
-    return "This task is from celery"
+@celery.task(name="create_task", serializer="json")
+def create_task(user_dict: dict, db_url):
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+    db = Session()
+    try:
+        db_user = crud.get_user_by_email(db, email=user_dict["email"])
+        if db_user:
+            db.close()
+            return None
+        user = crud.create_user(db=db, user=user_dict)
+        new_user = {
+            key: value
+            for key, value in user.__dict__.items()
+            if not key.startswith("_")
+        }
+        db.commit()
+    except:
+        db.rollback()
+        raise
+    finally:
+        db.close()
+    return new_user
+
+
+@celery.task(name="login_task", serializer="json")
+def login_task(email: str, password: str, db_url):
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+    db = Session()
+    try:
+        db_user = crud.validate_credentials(db=db, email=email, password=password)
+        if db_user is None:
+            msg = {"Details": "Please enter valid credentials"}
+        else:
+            msg = {"Details": "User logged in successfully"}
+    except:
+        db.rollback()
+        raise
+    finally:
+        db.close()
+    return msg
 
 
 # Dependency
@@ -36,15 +76,13 @@ def get_db():
 
 @app.post("/signup/", response_model=schemas.User)
 def sign_up(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
+    data = create_task.delay(user.dict(), str(db.bind.url))
+    if not data.get():
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+    return data.get()
 
 
 @app.post("/login/")
 def login(email: str, password: str, db: Session = Depends(get_db)):
-    db_user = crud.validate_credentials(db, email=email, password=password)
-    if db_user is None:
-        return {"Details": "Please enter valid credentials"}
-    return {"Details": "User logged in successfully"}
+    data = login_task.delay(email, password, str(db.bind.url))
+    return data.get()
--- sql_app/tests/test_sql_app.py
+++ sql_app/tests/test_sql_app.py
@@ -1,65 +1,36 @@
-import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from unittest import mock
+from unittest.mock import patch
 
-from ..database import Base
-from ..main import app, get_db
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from ..main import create_task, login_task
 
 
-def override_get_db():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
+@mock.patch("sql_app.main.crud.get_user_by_email")
+@mock.patch("sql_app.main.crud.create_user")
+def test_create_task(mock_create_user, mock_get_user_by_email):
+    mock_get_user_by_email.return_value = None
+    mock_user_dict = {"email": "test@example.com", "password": "password"}
+    mock_db_url = "sqlite:///test.db"
+
+    expected_output = {"id": 1, "email": "test@example.com", "password": "password"}
+    mock_create_user.return_value.__dict__ = expected_output
+
+    output = create_task(mock_user_dict, mock_db_url)
+    assert output == expected_output
+    mock_create_user.assert_called_once()
+    mock_get_user_by_email.assert_called_once()
 
 
-@pytest.fixture(scope="session")
-def db():
-    Base.metadata.create_all(bind=engine)
-    yield
-    Base.metadata.drop_all(bind=engine)
+def test_login_task():
+    email = "test@example.com"
+    password = "password"
+    db_url = "sqlite:///test.db"
 
+    # Mock the validate_credentials function to return a user
+    with patch("sql_app.main.crud.validate_credentials") as mock_validate_credentials:
+        mock_validate_credentials.return_value = {"id": 1, "email": email}
 
-@pytest.fixture()
-def client(db):
-    app.dependency_overrides[get_db] = override_get_db
-    with TestClient(app) as c:
-        yield c
+        # Call the login_task function
+        result = login_task(email, password, db_url)
 
-
-def test_create_user(client):
-    response = client.post(
-        "/signup/",
-        json={"email": "example@example.com", "password": "example", "name": "test"},
-    )
-    assert response.status_code == 200, response.text
-
-
-def test_create_user_with_same_email(client):
-    response = client.post(
-        "/signup/",
-        json={"email": "example@example.com", "password": "example", "name": "test"},
-    )
-    data = response.json()
-    assert data["detail"] == "Email already registered"
-
-
-def test_login(client):
-    response = client.post("/login/?email=example%40example.com&password=example")
-    data = response.json()
-    assert data["Details"] == "User logged in successfully"
-
-
-def test_login_invalid(client):
-    response = client.post("/login/?email=example%40example.com&password=examples")
-    data = response.json()
-    assert data["Details"] == "Please enter valid credentials"
+        # Assert that the result is as expected
+        assert result == {"Details": "User logged in successfully"}
